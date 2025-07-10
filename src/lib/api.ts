7d8eecafcf1affde63ba1f02e9bbc456894f5538
Plaban Datta <plabandatta2015@gmail.com>
2025-07-10 16:52:49 +0530

    Updated API route

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:7050/allycare/api';
+const API_BASE_URL = 'https://wyh-backend.onrender.com/allycare/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
